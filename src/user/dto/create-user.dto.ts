a2893aa25890cb280f066136d6383ab9009f5db7
Habynner <habynner.jeuel@gmail.com>
2024-09-24 16:21:17 -0300

    Criando toda estrutura de usuario.

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -8,25 +8,12 @@ import { Exclude, Expose } from 'class-transformer';
 export class CreateUserDto {
   id: string;
 
-  @Expose({ name: 'name' })
-  @IsString()
-  @IsNotEmpty({ message: 'The name should not be empty.' })
-  @IsNomeUsuarioUnico({ message: 'This user name alredy exist.' })
   nome: string;
 
-  @Expose({ name: 'userName' })
-  @IsString()
-  @IsNotEmpty({ message: 'The name should not be empty.' })
   nomeUsuario: string;
 
-  @IsEmail(undefined, { message: 'email must be e-mail.' })
-  @EmailEhUnico({ message: 'This user email alredy exist.' })
   email: string;
 
-  @Expose({ name: 'password' })
-  @Exclude({ toPlainOnly: true })
-  @MinLength(6, { message: 'The password do not has less then 6 characthers.' })
-  @IsNotEmpty({ message: 'A senha é obrigatória.' })
   senha: string;
 
   createdAt: string;
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -8,23 +8,12 @@ import {
 import { EmailEhUnico } from '../validator/email-unico.validator';
 
 export class UpdateUserDto {
-  @IsString()
-  @IsNotEmpty({ message: 'The name should not be empty.' })
-  @IsOptional()
   nome: string;
 
-  @IsString()
-  @IsNotEmpty({ message: 'The User name should not be empty.' })
-  @IsOptional()
   nomeUsuario: string;
 
-  @IsEmail(undefined, { message: 'email must be e-mail.' })
-  @EmailEhUnico({ message: 'This user email alredy exist.' })
-  @IsOptional()
   email: string;
 
-  @MinLength(6, { message: 'The password do not has less then 6 characthers.' })
-  @IsOptional()
   senha: string;
 
   createdAt: string;
